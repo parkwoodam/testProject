6a9363cec33ae30038dd59dd863886271ab6bcfe
sunny <rjqnrdl5113@naver.com>
2024-10-24 16:20:35 +0900

    2024-10-24

---
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,51 @@
+package hello.hello_spring.controller;
+
+import org.springframework.ui.Model;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.*;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hellO(Model model) {
+        model.addAttribute("data", "우담!!");
+        return "hello"; // resources:templetes/hello.html 렌더링해라!!!
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+}
